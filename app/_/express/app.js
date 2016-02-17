7bc0ddf9b1b902942ed0dabc5f70f4f8197c59b9
Juan C Irizar <juan@up3.com>
2016-02-17 16:58:18 -0500

    remove favicon dependency

--- app/_/express/app.js
+++ app/_/express/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,8 +14,7 @@ app.use(compression({
   },
   level: 9
 }));
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(helmet());
 app.use(bodyParser.json());
